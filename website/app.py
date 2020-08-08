a897d5602caa0b017e9a7a2578afdaff2cc18f7c
Éloi Rivard <eloi.rivard@aquilenet.fr>
2020-08-08 17:42:24 +0200

    Docker image and minimalistic configuration

--- website/app.py
+++ website/app.py
@@ -1,5 +1,6 @@
+import ldap
 import os
-from flask import Flask
+from flask import Flask, g
 from .models import db
 from .oauth2 import config_oauth
 from .routes import bp
@@ -9,17 +10,17 @@ def create_app(config=None):
     app = Flask(__name__)
 
     # load default configuration
-    app.config.from_object('website.settings')
+    app.config.from_object("website.settings")
 
     # load environment configuration
-    if 'WEBSITE_CONF' in os.environ:
-        app.config.from_envvar('WEBSITE_CONF')
+    if "WEBSITE_CONF" in os.environ:
+        app.config.from_envvar("WEBSITE_CONF")
 
     # load app specified configuration
     if config is not None:
         if isinstance(config, dict):
             app.config.update(config)
-        elif config.endswith('.py'):
+        elif config.endswith(".py"):
             app.config.from_pyfile(config)
 
     setup_app(app)
@@ -27,6 +28,17 @@ def create_app(config=None):
 
 
 def setup_app(app):
+    @app.before_request
+    def before_request():
+        g.ldap = ldap.initialize("ldap://ldap")
+        g.ldap.simple_bind_s("cn=admin,dc=mydomain,dc=tld", "admin")
+
+    @app.after_request
+    def after_request(response):
+        if "ldap" in g:
+            g.ldap.unbind_s()
+        return response
+
     # Create tables if they do not exist already
     @app.before_first_request
     def create_tables():
@@ -34,4 +46,4 @@ def setup_app(app):
 
     db.init_app(app)
     config_oauth(app)
-    app.register_blueprint(bp, url_prefix='')
+    app.register_blueprint(bp, url_prefix="")

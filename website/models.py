09c6236bedfe753514be11704829dfd1ac764e76
Éloi Rivard <eloi.rivard@aquilenet.fr>
2020-08-09 14:40:32 +0200

    Explicit model mixins

--- website/models.py
+++ website/models.py
@@ -1,10 +1,13 @@
 import time
-from flask_sqlalchemy import SQLAlchemy
-from authlib.integrations.sqla_oauth2 import (
-    OAuth2ClientMixin,
-    OAuth2AuthorizationCodeMixin,
-    OAuth2TokenMixin,
+from authlib.common.encoding import json_loads, json_dumps
+from authlib.oauth2.rfc6749.util import scope_to_list, list_to_scope
+from authlib.oauth2.rfc6749 import ClientMixin
+from authlib.oauth2.rfc6749 import (
+    TokenMixin,
+    AuthorizationCodeMixin,
 )
+from flask_sqlalchemy import SQLAlchemy
+
 
 db = SQLAlchemy()
 
@@ -23,7 +26,7 @@ class User(db.Model):
         return password == 'valid'
 
 
-class OAuth2Client(db.Model, OAuth2ClientMixin):
+class OAuth2Client(db.Model, ClientMixin):
     __tablename__ = 'oauth2_client'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -31,8 +34,135 @@ class OAuth2Client(db.Model, OAuth2ClientMixin):
         db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
     user = db.relationship('User')
 
+    client_id = db.Column(db.String(48), index=True)
+    client_secret = db.Column(db.String(120))
+    client_id_issued_at = db.Column(db.Integer, nullable=False, default=0)
+    client_secret_expires_at = db.Column(db.Integer, nullable=False, default=0)
+    _client_metadata = db.Column('client_metadata', db.Text)
 
-class OAuth2AuthorizationCode(db.Model, OAuth2AuthorizationCodeMixin):
+    @property
+    def client_info(self):
+        """Implementation for Client Info in OAuth 2.0 Dynamic Client
+        Registration Protocol via `Section 3.2.1`_.
+        .. _`Section 3.2.1`: https://tools.ietf.org/html/rfc7591#section-3.2.1
+        """
+        return dict(
+            client_id=self.client_id,
+            client_secret=self.client_secret,
+            client_id_issued_at=self.client_id_issued_at,
+            client_secret_expires_at=self.client_secret_expires_at,
+        )
+
+    @property
+    def client_metadata(self):
+        if 'client_metadata' in self.__dict__:
+            return self.__dict__['client_metadata']
+        if self._client_metadata:
+            data = json_loads(self._client_metadata)
+            self.__dict__['client_metadata'] = data
+            return data
+        return {}
+
+    def set_client_metadata(self, value):
+        self._client_metadata = json_dumps(value)
+
+    @property
+    def redirect_uris(self):
+        return self.client_metadata.get('redirect_uris', [])
+
+    @property
+    def token_endpoint_auth_method(self):
+        return self.client_metadata.get(
+            'token_endpoint_auth_method',
+            'client_secret_basic'
+        )
+
+    @property
+    def grant_types(self):
+        return self.client_metadata.get('grant_types', [])
+
+    @property
+    def response_types(self):
+        return self.client_metadata.get('response_types', [])
+
+    @property
+    def client_name(self):
+        return self.client_metadata.get('client_name')
+
+    @property
+    def client_uri(self):
+        return self.client_metadata.get('client_uri')
+
+    @property
+    def logo_uri(self):
+        return self.client_metadata.get('logo_uri')
+
+    @property
+    def scope(self):
+        return self.client_metadata.get('scope', '')
+
+    @property
+    def contacts(self):
+        return self.client_metadata.get('contacts', [])
+
+    @property
+    def tos_uri(self):
+        return self.client_metadata.get('tos_uri')
+
+    @property
+    def policy_uri(self):
+        return self.client_metadata.get('policy_uri')
+
+    @property
+    def jwks_uri(self):
+        return self.client_metadata.get('jwks_uri')
+
+    @property
+    def jwks(self):
+        return self.client_metadata.get('jwks', [])
+
+    @property
+    def software_id(self):
+        return self.client_metadata.get('software_id')
+
+    @property
+    def software_version(self):
+        return self.client_metadata.get('software_version')
+
+    def get_client_id(self):
+        return self.client_id
+
+    def get_default_redirect_uri(self):
+        if self.redirect_uris:
+            return self.redirect_uris[0]
+
+    def get_allowed_scope(self, scope):
+        if not scope:
+            return ''
+        allowed = set(self.scope.split())
+        scopes = scope_to_list(scope)
+        return list_to_scope([s for s in scopes if s in allowed])
+
+    def check_redirect_uri(self, redirect_uri):
+        return redirect_uri in self.redirect_uris
+
+    def has_client_secret(self):
+        return bool(self.client_secret)
+
+    def check_client_secret(self, client_secret):
+        return self.client_secret == client_secret
+
+    def check_token_endpoint_auth_method(self, method):
+        return self.token_endpoint_auth_method == method
+
+    def check_response_type(self, response_type):
+        return response_type in self.response_types
+
+    def check_grant_type(self, grant_type):
+        return grant_type in self.grant_types
+
+
+class OAuth2AuthorizationCode(db.Model, AuthorizationCodeMixin):
     __tablename__ = 'oauth2_code'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -40,8 +170,37 @@ class OAuth2AuthorizationCode(db.Model, OAuth2AuthorizationCodeMixin):
         db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
     user = db.relationship('User')
 
+    code = db.Column(db.String(120), unique=True, nullable=False)
+    client_id = db.Column(db.String(48))
+    redirect_uri = db.Column(db.Text, default='')
+    response_type = db.Column(db.Text, default='')
+    scope = db.Column(db.Text, default='')
+    nonce = db.Column(db.Text)
+    auth_time = db.Column(
+        db.Integer, nullable=False,
+        default=lambda: int(time.time())
+    )
 
-class OAuth2Token(db.Model, OAuth2TokenMixin):
+    code_challenge = db.Column(db.Text)
+    code_challenge_method = db.Column(db.String(48))
+
+    def is_expired(self):
+        return self.auth_time + 300 < time.time()
+
+    def get_redirect_uri(self):
+        return self.redirect_uri
+
+    def get_scope(self):
+        return self.scope
+
+    def get_auth_time(self):
+        return self.auth_time
+
+    def get_nonce(self):
+        return self.nonce
+
+
+class OAuth2Token(db.Model, TokenMixin):
     __tablename__ = 'oauth2_token'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -49,8 +208,31 @@ class OAuth2Token(db.Model, OAuth2TokenMixin):
         db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
     user = db.relationship('User')
 
+    client_id = db.Column(db.String(48))
+    token_type = db.Column(db.String(40))
+    access_token = db.Column(db.String(255), unique=True, nullable=False)
+    refresh_token = db.Column(db.String(255), index=True)
+    scope = db.Column(db.Text, default='')
+    revoked = db.Column(db.Boolean, default=False)
+    issued_at = db.Column(
+        db.Integer, nullable=False, default=lambda: int(time.time())
+    )
+    expires_in = db.Column(db.Integer, nullable=False, default=0)
+
     def is_refresh_token_active(self):
         if self.revoked:
             return False
         expires_at = self.issued_at + self.expires_in * 2
         return expires_at >= time.time()
+
+    def get_client_id(self):
+        return self.client_id
+
+    def get_scope(self):
+        return self.scope
+
+    def get_expires_in(self):
+        return self.expires_in
+
+    def get_expires_at(self):
+        return self.issued_at + self.expires_in
--- website/oauth2.py
+++ website/oauth2.py
@@ -2,12 +2,6 @@ from authlib.integrations.flask_oauth2 import (
     AuthorizationServer,
     ResourceProtector,
 )
-from authlib.integrations.sqla_oauth2 import (
-    create_query_client_func,
-    create_save_token_func,
-    create_revocation_endpoint,
-    create_bearer_token_validator,
-)
 from authlib.oauth2.rfc6749 import grants
 from authlib.oauth2.rfc7636 import CodeChallenge
 from .models import db, User
@@ -16,14 +10,14 @@ from .models import OAuth2Client, OAuth2AuthorizationCode, OAuth2Token
 
 class AuthorizationCodeGrant(grants.AuthorizationCodeGrant):
     TOKEN_ENDPOINT_AUTH_METHODS = [
-        'client_secret_basic',
-        'client_secret_post',
-        'none',
+        "client_secret_basic",
+        "client_secret_post",
+        "none",
     ]
 
     def save_authorization_code(self, code, request):
-        code_challenge = request.data.get('code_challenge')
-        code_challenge_method = request.data.get('code_challenge_method')
+        code_challenge = request.data.get("code_challenge")
+        code_challenge_method = request.data.get("code_challenge_method")
         auth_code = OAuth2AuthorizationCode(
             code=code,
             client_id=request.client.client_id,
@@ -39,7 +33,8 @@ class AuthorizationCodeGrant(grants.AuthorizationCodeGrant):
 
     def query_authorization_code(self, code, client):
         auth_code = OAuth2AuthorizationCode.query.filter_by(
-            code=code, client_id=client.client_id).first()
+            code=code, client_id=client.client_id
+        ).first()
         if auth_code and not auth_code.is_expired():
             return auth_code
 
@@ -73,12 +68,46 @@ class RefreshTokenGrant(grants.RefreshTokenGrant):
         db.session.commit()
 
 
+def create_query_client_func(session, client_model):
+    """Create an ``query_client`` function that can be used in authorization
+    server.
+    :param session: SQLAlchemy session
+    :param client_model: Client model class
+    """
+
+    def query_client(client_id):
+        q = session.query(client_model)
+        return q.filter_by(client_id=client_id).first()
+
+    return query_client
+
+
 query_client = create_query_client_func(db.session, OAuth2Client)
+
+
+def create_save_token_func(session, token_model):
+    """Create an ``save_token`` function that can be used in authorization
+    server.
+    :param session: SQLAlchemy session
+    :param token_model: Token model class
+    """
+
+    def save_token(token, request):
+        if request.user:
+            user_id = request.user.get_user_id()
+        else:
+            user_id = None
+        client = request.client
+        item = token_model(client_id=client.client_id, user_id=user_id, **token)
+        session.add(item)
+        session.commit()
+
+    return save_token
+
+
 save_token = create_save_token_func(db.session, OAuth2Token)
-authorization = AuthorizationServer(
-    query_client=query_client,
-    save_token=save_token,
-)
+
+authorization = AuthorizationServer(query_client=query_client, save_token=save_token,)
 require_oauth = ResourceProtector()
 
 
@@ -93,9 +122,73 @@ def config_oauth(app):
     authorization.register_grant(RefreshTokenGrant)
 
     # support revocation
+    def create_revocation_endpoint(session, token_model):
+        """Create a revocation endpoint class with SQLAlchemy session
+        and token model.
+        :param session: SQLAlchemy session
+        :param token_model: Token model class
+        """
+        from authlib.oauth2.rfc7009 import RevocationEndpoint
+
+        def create_query_token_func(session, token_model):
+            """Create an ``query_token`` function for revocation, introspection
+            token endpoints.
+            :param session: SQLAlchemy session
+            :param token_model: Token model class
+            """
+
+            def query_token(token, token_type_hint, client):
+                q = session.query(token_model)
+                q = q.filter_by(client_id=client.client_id, revoked=False)
+                if token_type_hint == "access_token":
+                    return q.filter_by(access_token=token).first()
+                elif token_type_hint == "refresh_token":
+                    return q.filter_by(refresh_token=token).first()
+                # without token_type_hint
+                item = q.filter_by(access_token=token).first()
+                if item:
+                    return item
+                return q.filter_by(refresh_token=token).first()
+
+            return query_token
+
+        query_token = create_query_token_func(session, token_model)
+
+        class _RevocationEndpoint(RevocationEndpoint):
+            def query_token(self, token, token_type_hint, client):
+                return query_token(token, token_type_hint, client)
+
+            def revoke_token(self, token):
+                token.revoked = True
+                session.add(token)
+                session.commit()
+
+        return _RevocationEndpoint
+
     revocation_cls = create_revocation_endpoint(db.session, OAuth2Token)
     authorization.register_endpoint(revocation_cls)
 
     # protect resource
+    def create_bearer_token_validator(session, token_model):
+        """Create an bearer token validator class with SQLAlchemy session
+        and token model.
+        :param session: SQLAlchemy session
+        :param token_model: Token model class
+        """
+        from authlib.oauth2.rfc6750 import BearerTokenValidator
+
+        class _BearerTokenValidator(BearerTokenValidator):
+            def authenticate_token(self, token_string):
+                q = session.query(token_model)
+                return q.filter_by(access_token=token_string).first()
+
+            def request_invalid(self, request):
+                return False
+
+            def token_revoked(self, token):
+                return token.revoked
+
+        return _BearerTokenValidator
+
     bearer_cls = create_bearer_token_validator(db.session, OAuth2Token)
     require_oauth.register_token_validator(bearer_cls())
